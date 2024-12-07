41dc356df735fdf885cce254ba8bd4e226602707
FabianAster <fabast01@gmail.com>
2024-12-07 17:13:40 +0100

    updated runall

--- RLapproxProject/runAll.sh
+++ RLapproxProject/runAll.sh
@@ -1,5 +1,9 @@
-
 bash run.sh CartPoleSarsa
 bash run.sh CartPoleMonteCarlo
 bash run.sh CartPoleReinforce
-# bash run.sh CartPoleReinforceBaseline
+bash run.sh CartPoleReinforceBaseline
+
+python plots.py CartPoleSarsa
+python plots.py CartPoleMonteCarlo
+python plots.py CartPoleReinforce
+python plots.py CartPoleReinforceBaseline
